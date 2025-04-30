086703c8411418640c10dc9734dace14a61aa003
LinJie9 <2983606672@qq.com>
2025-04-30 14:02:09 +0800

    first commit

---
+++ api/http.js
@@ -0,0 +1,43 @@
+let baseUrl = ''
+
+
+if(process.env.NODE_ENV === 'development'){
+	baseUrl = 'http://localhost:5173/api'
+}else{
+	// baseUrl = 'https://apifoxmock.com/m1/4728220-0-default/api'
+	baseUrl = 'https://apifoxmock.com/m1/4728220-0-default/api'
+}
+
+export default function http (url, data ={}, method = 'GET'){
+	
+	//对外链式调用
+	return new Promise((resolve, reject) => {
+		uni.request({
+			url: baseUrl + url,
+			data,
+			method,
+			header: {
+				'token': uni.getStorageSync('token') || ''
+			},
+			success: res => {
+				if(res.statusCode == 200){
+					if(res.data.code == 1){
+						resolve(res.data.data)
+					}else if(res.data.code == 0){
+						uni.showToast({
+							title:res.data.msg,
+							icon:'none'
+						})
+						reject(res.data.msg)
+					}
+				}
+			},
+			fail:() => {
+				uni.showToast({
+					title:'服务器请求异常',
+					icon:'none'
+				})
+			}
+		})
+	})
+}
---
+++ pages/index/index.vue
@@ -0,0 +1,26 @@
+<template>
+	<view class="">
+		
+		
+		<up-button text="月落"></up-button>
+		
+	</view>
+</template>
+
+<script setup>
+	import { getBanner } from '../../api/api.js';
+	import { onLoad } from '@dcloudio/uni-app'
+	
+	
+	onLoad(() => {
+		getBanner().then(res => {
+			console.log(res, 'res');
+		})
+	})
+	
+	
+</script>
+
+<style>
+	
+</style>
