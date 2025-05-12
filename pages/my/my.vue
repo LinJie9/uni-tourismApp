68bf1eacf25fd9e972d543ddb5b62455a08dc365
LinJie9 <2983606672@qq.com>
2025-05-12 22:16:52 +0800

    写了我的页面的内容“

--- pages/my/my.vue
+++ pages/my/my.vue
@@ -15,23 +15,68 @@
 						<uni-icons type="chat" size="30" color="#fff"></uni-icons>
 					</view>
 				</view>
+				
+				
+				<view class="users">
+					<view class="u-top">
+						<template v-if="!userInfo.nickName">
+							<image src="../../static/tt.jpg" mode="aspectFill"></image>
+							<view class="tit">注册 / 登录</view>
+						</template>
+						
+						<template v-else>
+							<image :src="userInfo.avatarUrl" mode="aspectFill"></image>
+							<view class="tit">{{userInfo.nickName}}</view>
+						</template>
+					</view>
+					
+					
+					<view class="u-bottom">
+						<view class="u-item">
+							<view class="num">9</view>
+							<view class="u-tit">点赞</view>
+						</view>
+						<view class="u-item">
+							<view class="num">9</view>
+							<view class="u-tit">喜欢</view>
+						</view>
+						<view class="u-item">
+							<view class="num">9</view>
+							<view class="u-tit">浏览</view>
+						</view>
+						<view class="u-item">
+							<view class="num">9</view>
+							<view class="u-tit">收藏</view>
+						</view>
+					</view>
+				</view>
+				
+				
+				
+				
 			</view>
-			
-			
-			
 		</view>
 		
 	</view>
 </template>
 
-<script>
-	export default {
-		data() {
-			return {
+<script setup>
+import {ref, reactive } from 'vue'
+
+const userInfo = reactive({
+	nickName:'',
+	avatarUrl:''
+})
+	
+	
+	// export default {
+	// 	data() {
+	// 		return {
 				
-			};
-		}
-	}
+	// 		};
+	// 	}
+	// }
+	
 </script>
 
 <style lang="scss" scoped>
@@ -73,5 +118,52 @@
 			font-size: 30rpx;
 		}
 	}
+	
+	.users {
+		margin-top: 35rpx;
+		padding: 30rpx;
+		box-sizing: border-box;
+		height: 280rpx;
+		background-color: #fff;
+		box-shadow: 1px 10rpx 20rpx #ececec;
+		border-radius: 16rpx;
+		.u-top {
+			display: flex;
+			justify-content: flex-start;
+			align-items: center;
+			margin-bottom: 30rpx;
+			image {
+				width: 100rpx;
+				height: 100rpx;
+				border-radius: 50%;
+				margin-right: 20rpx;
+			}
+			.tit {
+				font-size: 30rpx;
+				font-weight: 700;
+				color: #333;
+			}
+		}
+		.u-bottom{
+			display: flex;
+			justify-content: space-around;
+			align-items: center;
+			.u-item {
+				text-align: center;
+				.u-tit {
+					color: #757575;
+					font-size: 26rpx;
+					margin-top: 10rpx;
+				}
+				.num {
+					color: #000;
+					font-size: 33rpx;
+					font-weight: 700;
+				}
+			}
+		}
+	}
+	
+	
 
 </style>
